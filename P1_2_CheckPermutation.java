621d4cff3997530926e8f64122c9d54fd8934e81
phuongdoan13 <mphuongdoant@gmail.com>
2020-03-19 15:47:21 +1100

    Add P1_2

--- P1_2_CheckPermutation.java
+++ P1_2_CheckPermutation.java
@@ -1,15 +1,44 @@
 import java.util.*;
+
 public class P1_2_CheckPermutation{
     //Problem: Implemennt an algorithm to determine 
     //if a string has all unique charcters. 
     //What if you cannot use additional data structures?
-    public static void main(String[] args) {
-       System.out.println(isPermutated("abcdefg", "asiodfhoa"));
-    }
-    public static boolean isPermutated(String s1, String s2) {
-        //
-        
     
-        return true;
+    public static void main(String[] args) {
+       System.out.println(isPermutatedSort("abcdefg", "abcdegf"));
+       System.out.println(isPermutatedCount("abc", "cba"));
+    }
+
+    // Sorting way //
+    static boolean isPermutatedSort(String s1, String s2) {
+        // 
+        
+        if(s1.length() != s2.length()) return false;
+        return sortStr(s1).equals(sortStr(s2));
     } 
+    static String sortStr(String str){
+        char[] strArray = str.toCharArray(); // O(n)
+        Arrays.sort(strArray); //O(nlogn)
+        return new String(strArray);
+    }
+
+    // Identical Character counts
+    static boolean isPermutatedCount(String s1, String s2){
+        if(s1.length() != s2.length()) return false;
+        
+        int[] characters = new int[128];
+        char[] s1Array = s1.toCharArray();
+        
+        for (char c1: s1Array){
+            characters[c1]++;
+        }
+        
+        for (int i = 0; i < s2.length(); i++){
+            int c2 = (int) s2.charAt(i);
+            characters[c2]--;
+            if(characters[c2] < 0) return false;
+        }
+        return true;
+    }
 }
